c842884f6daf6f07ee57b69e0c24707ba078b654
ForCM <162288752+lelembut599@users.noreply.github.com>
2024-10-09 22:24:53 +0800

    POSTTEST 5

---
+++ Posttest 5/form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+    
+    $nama = htmlspecialchars($_GET['nama']);
+    $email = htmlspecialchars($_GET['email']);
+    $telp = htmlspecialchars($_GET['telp']);
+    $komen = htmlspecialchars($_GET['komen']);
+    
+    echo "Nama      : $nama<br>";
+    echo "E-mail    : $email<br>";
+    echo "Telp      : $telp<br>";
+    echo "Komentar  : $komen<br>";
+     ?>
+</body>
+</html>
